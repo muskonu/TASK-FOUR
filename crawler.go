8886cf1aca46142865e488a7d8814c6e2a176bac
muskonu <117137903+muskonu@users.noreply.github.com>
2023-01-11 22:17:03 +0800

    Update crawler.go

--- crawler.go
+++ crawler.go
@@ -4,12 +4,23 @@ import (
 	"fmt"
 	"ggggggggggggo/pkg"
 	"github.com/gocolly/colly"
-	"strconv"
 )
 
 func Crawl(url string) {
+	var number uint = uint(pkg.ReadNumber())
 	c := colly.NewCollector()
-	var books []pkg.Book
+	db := pkg.CreateDb()
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Visited", r.Request.URL)
+	})
+	c.OnHTML("#category a[href]", func(e *colly.HTMLElement) {
+		if e.Attr("href") != "/dianzishuzhizuo/" {
+			e.Request.Visit(e.Attr("href"))
+		}
+	})
+	c.OnHTML("div.pagenavi a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
 	c.OnHTML("div.channel-item", func(e *colly.HTMLElement) {
 		sbook := pkg.SeBook{}
 		err := e.Unmarshal(&sbook)
@@ -19,35 +30,17 @@ func Crawl(url string) {
 		}
 		book := pkg.Book{}
 		book.SeBook = sbook
-		books = append(books, book)
+		book.Id = number
+		err = db.Save(&book).Error
+		if err == nil {
+			number++
+		}
 	})
 	err := c.Visit(url)
 	if err != nil {
 		fmt.Println("Visit error:", err)
 		return
 	}
-	db := pkg.CreateDb()
-	db.Create(&books)
-} //爬取该页面的作者，书名以及分类
+	pkg.WriteNumber(int(number))
+} //爬取该页面的作者，书名以及分页
 
-func CrawlPage(i int, page chan<- int) {
-	var url string
-	if i == 1 {
-		url = "https://kgbook.com/shigesanwen/"
-	} else {
-		url = "https://kgbook.com/shigesanwen/index_" + strconv.Itoa(i) + ".html"
-	}
-	Crawl(url)
-	page <- i
-} //同时爬取不同页面
-
-func Run(start, end int) {
-	fmt.Printf("正在爬取第%d页到%d页\n", start, end)
-	page := make(chan int)
-	for i := start; i <= end; i++ {
-		go CrawlPage(i, page)
-	}
-	for i := start; i <= end; i++ {
-		fmt.Printf("第%d个页面爬取完成\n", <-page) //防止爬虫还没有结束函数就退出
-	}
-}
